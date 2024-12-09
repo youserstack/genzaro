f084457e58136754266447318a6e32524504ad50
youserstack <youserstack@gmail.com>
2024-12-09 09:34:48 +0900

    update

--- src/components/header/Nav.tsx
+++ src/components/header/Nav.tsx
@@ -2,25 +2,96 @@ import { useRef, useState } from "react";
 
 const details = [
   {
-    title: "fashion1",
-    items: [{ name: "1111111" }, { name: "1111111" }],
-  },
-  {
-    title: "fashion2",
-    items: [{ name: "222222" }, { name: "222222" }, { name: "222222" }, { name: "222222" }],
-  },
-  {
-    title: "fashion3",
+    title: "Electronics",
     items: [
-      { name: "333333" },
-      { name: "333333" },
-      { name: "333333" },
-      { name: "333333" },
-      { name: "333333" },
-      { name: "333333" },
-      { name: "333333" },
-      { name: "333333" },
-      { name: "333333" },
+      {
+        name: "Computers",
+        items: [
+          { name: "컴퓨터" },
+          { name: "태블릿" },
+          { name: "저장장치" },
+          { name: "모니터" },
+          { name: "컴퓨터 부품" },
+          { name: "액세서리" },
+        ],
+      },
+      {
+        name: "Smart Home",
+        items: [
+          { name: "조명" },
+          { name: "로봇청소기" },
+          { name: "보안 시스템" },
+          { name: "보이스 어시스턴트" },
+          { name: "플러그" },
+          { name: "부엌" },
+        ],
+      },
+      { name: "Headphones" },
+      { name: "Audio" },
+      { name: "Car Electronics" },
+      { name: "Accessories" },
+    ],
+  },
+  {
+    title: "Fashion",
+    items: [
+      {
+        name: "Clothing",
+        items: [
+          { name: "자켓" },
+          { name: "코트" },
+          { name: "패딩" },
+          { name: "티셔츠" },
+          { name: "스웨트셔츠" },
+          { name: "바지" },
+          { name: "청바지" },
+          { name: "반바지" },
+          { name: "이너웨어" },
+          { name: "스포츠웨어" },
+        ],
+      },
+      {
+        name: "Shoes",
+        items: [
+          { name: "운동화" },
+          { name: "스니커즈" },
+          { name: "런닝화" },
+          { name: "부츠" },
+          { name: "옥스퍼드화" },
+          { name: "로퍼" },
+          { name: "슬립온" },
+          { name: "샌들" },
+          { name: "슬리퍼" },
+          { name: "작업화 및 안전화" },
+        ],
+      },
+      { name: "Accessories" },
+    ],
+  },
+  {
+    title: "Home & Kitchen",
+    items: [
+      {
+        name: "Furniture",
+        items: [
+          //
+          { name: "소파" },
+          { name: "침대" },
+          { name: "의자" },
+        ],
+      },
+      {
+        name: "Kitchen & Dining",
+        items: [
+          //
+          { name: "주방용품" },
+          { name: "조리기구" },
+          { name: "식기" },
+          { name: "제빵기구" },
+        ],
+      },
+      { name: "Cleaning", items: [{ name: "무선청소기" }, { name: "공기청정기" }] },
+      { name: "Storage", items: [{ name: "수납 및 정리" }] },
     ],
   },
 ];
@@ -73,15 +144,26 @@ export default function Nav() {
         >
           <div
             className="Padding_Layer
-            p-10 border border-white"
+            p-8 border border-white flex gap-4"
           >
-            <ul>
+            {/* <ul className="border border-green-500">
               {detail?.items.map((item, index) => (
                 <li key={index} className="">
                   {item.name}
                 </li>
               ))}
-            </ul>
+            </ul> */}
+            {detail?.items.map((item, index) => (
+              <div key={index}>
+                <h5>{item.name}</h5>
+
+                <ul className="border border-green-500">
+                  {item.items?.map((item, index) => (
+                    <li key={index}>{item.name}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
           </div>
         </div>
       </div>
