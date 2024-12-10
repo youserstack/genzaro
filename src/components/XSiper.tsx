777a5e69045876bcdcc8d433945781f6f42873b3
youserstack <youserstack@gmail.com>
2024-12-10 16:25:50 +0900

    update

--- src/components/XSiper.tsx
+++ src/components/XSiper.tsx
@@ -7,6 +7,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/parallax";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 const items = [{ bgUrl: "", imageUrl: "" }];
 
@@ -34,67 +35,96 @@ const Background = () => {
 export default function XSiper() {
   return (
     <Swiper
-      slidesPerView={1.5}
-      // spaceBetween={70}
       modules={[Navigation, Pagination, Keyboard, Parallax, Autoplay]}
-      navigation={true}
-      pagination={{ clickable: true }}
+      navigation={{
+        //
+        enabled: true,
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      }}
+      pagination={{
+        el: ".swiper-pagination",
+        type: "bullets", // 기본값
+        clickable: true, // 점 클릭 시 슬라이드 이동 가능
+        renderBullet: (index, className) => `<span class="${className} !bg-white"></span>`,
+      }}
       keyboard={{ enabled: true }}
+      slidesPerView={1.5}
+      speed={700}
       centeredSlides
       parallax
       loop
-      speed={700}
-      // setWrapperSize
-      // height={300}
 
+      // spaceBetween={70}
       // autoplay={{ delay: 1000 }}
       // onSlideChange={() => console.log("slide change")}
       // onSwiper={(swiper) => console.log(swiper)}
     >
-      <SwiperSlide>
-        <Background />
-        <Image
-          src={"https://shopping-phinf.pstatic.net/main_4683155/46831553621.20240403112809.jpg"}
-          alt=""
-          width={300}
-          height={300}
-          className="w-full h-full object-center object-cover"
-          data-swiper-parallax="-50%"
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Background />
-        <Image
-          src={"https://shopping-phinf.pstatic.net/main_4683155/46831553621.20240403112809.jpg"}
-          alt=""
-          width={400}
-          height={400}
-          className="w-full "
-          data-swiper-parallax="-50%"
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Background />
-        <Image
-          src={"https://shopping-phinf.pstatic.net/main_4683155/46831553621.20240403112809.jpg"}
-          alt=""
-          width={400}
-          height={400}
-          className="w-full h-full"
-          data-swiper-parallax="-50%"
-        />
-      </SwiperSlide>
-      <SwiperSlide>
-        <Background />
-        <Image
-          src={"https://shopping-phinf.pstatic.net/main_4683155/46831553621.20240403112809.jpg"}
-          alt=""
-          width={400}
-          height={400}
-          className="w-full h-full"
-          data-swiper-parallax="-50%"
-        />
-      </SwiperSlide>
+      {/* swiper slides */}
+      <div>
+        <SwiperSlide>
+          <Background />
+          <Image
+            src={"https://shopping-phinf.pstatic.net/main_4683155/46831553621.20240403112809.jpg"}
+            alt=""
+            width={300}
+            height={300}
+            className="w-full h-full object-center object-cover"
+            data-swiper-parallax="-50%"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Background />
+          <Image
+            src={"https://shopping-phinf.pstatic.net/main_4683155/46831553621.20240403112809.jpg"}
+            alt=""
+            width={400}
+            height={400}
+            className="w-full "
+            data-swiper-parallax="-50%"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Background />
+          <Image
+            src={"https://shopping-phinf.pstatic.net/main_4683155/46831553621.20240403112809.jpg"}
+            alt=""
+            width={400}
+            height={400}
+            className="w-full h-full"
+            data-swiper-parallax="-50%"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Background />
+          <Image
+            src={"https://shopping-phinf.pstatic.net/main_4683155/46831553621.20240403112809.jpg"}
+            alt=""
+            width={400}
+            height={400}
+            className="w-full h-full"
+            data-swiper-parallax="-50%"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Background />
+          <Image
+            src={"https://shopping-phinf.pstatic.net/main_4683155/46831553621.20240403112809.jpg"}
+            alt=""
+            width={400}
+            height={400}
+            className="w-full h-full"
+            data-swiper-parallax="-50%"
+          />
+        </SwiperSlide>
+      </div>
+
+      {/* navigation arrow buttons */}
+      <IoIosArrowBack className="swiper-button-prev !text-white" />
+      <IoIosArrowForward className="swiper-button-next !text-white" />
+
+      {/* pagination bullet buttons */}
+      <div className="swiper-pagination !bottom-[10%]"></div>
     </Swiper>
   );
 }
