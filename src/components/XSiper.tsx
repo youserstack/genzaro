c6bf9a92ba11d1bfcd6da2ff8ade8dd2a0d5a6ab
youserstack <youserstack@gmail.com>
2024-12-10 13:46:35 +0900

    update

--- src/components/XSiper.tsx
+++ src/components/XSiper.tsx
@@ -8,12 +8,35 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/parallax";
 
+const url =
+  "https://blog-next-app.tooroo.rf.gd/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdzktdrw7o%2Fimage%2Fupload%2Fv1731244673%2Fblog-next-app%2Fjvbqapd33yo6sz8zdrgi.webp&w=1080&q=75";
+
+const Background = () => {
+  return (
+    <div className="absolute inset-0 overflow-hidden rounded-lg">
+      {/* <div
+        className="w-[130%] h-full bg-no-repeat bg-cover bg-center 
+        /transform /-translate-x-[10%]"
+        style={{ backgroundImage: `url(${url})` }}
+        data-swiper-parallax="-10%"
+        // data-swiper-parallax="-100"
+        // data-swiper-parallax-scale="0.5"
+      /> */}
+      <img
+        src={url}
+        className="w-[130%] max-w-[130%] h-full object-center object-cover absolute left-[-15%]"
+        data-swiper-parallax="-10%"
+      />
+    </div>
+  );
+};
+
 export default function XSiper() {
   return (
     <div>
       <Swiper
         slidesPerView={1.5}
-        spaceBetween={50}
+        spaceBetween={70}
         modules={[Navigation, Pagination, Keyboard, Parallax, Autoplay]}
         navigation={true}
         pagination={{ clickable: true }}
@@ -21,69 +44,54 @@ export default function XSiper() {
         centeredSlides
         parallax
         loop
+        speed={2000}
 
         // autoplay={{ delay: 1000 }}
         // onSlideChange={() => console.log("slide change")}
         // onSwiper={(swiper) => console.log(swiper)}
       >
-        <div
-          slot="container-start"
-          className="parallax-bg"
-          style={{
-            backgroundImage:
-              "url(https://blog-next-app.tooroo.rf.gd/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdzktdrw7o%2Fimage%2Fupload%2Fv1731244673%2Fblog-next-app%2Fjvbqapd33yo6sz8zdrgi.webp&w=1080&q=75)",
-          }}
-          data-swiper-parallax="-23%"
-        ></div>
-
-        <SwiperSlide className="bg-slate-200">
+        <SwiperSlide className="/bg-slate-300 ">
+          <Background />
           <Image
             src={"https://shopping-phinf.pstatic.net/main_4683155/46831553621.20240403112809.jpg"}
             alt=""
             width={400}
             height={400}
             className="w-full"
-            data-swiper-parallax="-200"
+            data-swiper-parallax="-50%"
           />
         </SwiperSlide>
-        <SwiperSlide className="bg-slate-200">
+        <SwiperSlide className="/bg-slate-300 ">
+          <Background />
           <Image
             src={"https://shopping-phinf.pstatic.net/main_4683155/46831553621.20240403112809.jpg"}
             alt=""
             width={400}
             height={400}
             className="w-full"
-            data-swiper-parallax="-200"
+            data-swiper-parallax="-50%"
           />
         </SwiperSlide>
-        <SwiperSlide className="bg-slate-200">
+        <SwiperSlide className="/bg-slate-300 ">
+          <Background />
           <Image
             src={"https://shopping-phinf.pstatic.net/main_4683155/46831553621.20240403112809.jpg"}
             alt=""
             width={400}
             height={400}
             className="w-full"
-            data-swiper-parallax="-200"
+            data-swiper-parallax="-50%"
           />
         </SwiperSlide>
-        <SwiperSlide className="bg-slate-200">
+        <SwiperSlide className="/bg-slate-300 ">
+          <Background />
           <Image
             src={"https://shopping-phinf.pstatic.net/main_4683155/46831553621.20240403112809.jpg"}
             alt=""
             width={400}
             height={400}
             className="w-full"
-            data-swiper-parallax="-200"
-          />
-        </SwiperSlide>
-        <SwiperSlide className="bg-slate-200">
-          <Image
-            src={"https://shopping-phinf.pstatic.net/main_4683155/46831553621.20240403112809.jpg"}
-            alt=""
-            width={400}
-            height={400}
-            className="w-full"
-            data-swiper-parallax="-200"
+            data-swiper-parallax="-50%"
           />
         </SwiperSlide>
       </Swiper>
