aab17e6e12f849c666680ff602af213b5fead9f8
youserstack <youserstack@gmail.com>
2025-01-03 11:34:34 +0900

    update

--- src/utils/createOrder.ts
+++ src/utils/createOrder.ts
@@ -1,11 +1,18 @@
 export async function postOrder(order: Order) {
   try {
+    // const res = await fetch(`http://localhost:7000/api/order`, {
+    //   method: "post",
+    //   headers: { "Content-Type": "application/json" },
+    //   body: JSON.stringify(order),
+    //   // 크로스오리진이면 쿠키를 자동으로 보내지않아서 포함하겠다라는 명시를 해야한다.
+    //   credentials: "include",
+    // });
+
     const res = await fetch(`/api/order`, {
       method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(order),
+      //   세임오리진이면 쿠키를 자동으로 보낸다.
       //   credentials: "include",
     });
     if (!res.ok) throw new Error("주문요청실패(앱서버:express)");
